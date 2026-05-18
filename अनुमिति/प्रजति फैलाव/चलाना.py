8d4c20f99e309d786ecc5c4350d324f51a024e8b
Julien Malard-Adam <julien.malard@mail.mcgill.ca>
2026-05-18 15:57:05 +0530

    प्रगति

--- अनुमिति/प्रजति फैलाव/चलाना.py
+++ अनुमिति/प्रजति फैलाव/चलाना.py
@@ -1,4 +1,3 @@
-# https://www.pymc.io/projects/examples/en/stable/case_studies/occupancy.html
 import pymc_extras as pmx
 
 import os
@@ -11,8 +10,72 @@ import pandas as pd
 import pymc as pm
 from matplotlib.ticker import PercentFormatter
 from matplotlib import colors
-
+from उपकरण import scale
 rng = np.random.default_rng()
+
+try:
+    mhb_data = pd.read_csv(os.path.join("..", "data", "crossbill.csv"))
+except FileNotFoundError:
+    mhb_data = pd.read_csv(pm.get_data("crossbill.csv"))
+
+
+year = 2001
+
+try:
+    mhb_data = pd.read_csv(os.path.join("..", "data", "crossbill.csv"))
+except FileNotFoundError:
+    mhb_data = pd.read_csv(pm.get_data("crossbill.csv"))
+
+year = 2001
+
+# remove quadrats with missing surveys
+is_y_column = mhb_data.columns.str.startswith(f"det{str(year)[2:]}")
+y = mhb_data.loc[:, is_y_column].values
+was_surveyed = ~np.isnan(y).any(axis=1)
+y = y[was_surveyed].astype(int)
+quadrat_count, survey_count = y.shape
+
+# extract our covariates
+elevation = mhb_data["ele"].values[was_surveyed]
+forest_cover = mhb_data["forest"].values[was_surveyed]
+is_date_column = mhb_data.columns.str.startswith(f"date{str(year)[2:]}")
+date = mhb_data.loc[:, is_date_column].values[was_surveyed]
+
+# prepare design matrix for the occurrence model
+elev_scaled = scale(elevation)
+forest_scaled = scale(forest_cover)
+elev_scaled2 = elev_scaled ** 2
+X = np.column_stack((np.ones_like(elevation), forest_scaled, elev_scaled, elev_scaled2))
+
+# some dates are missing so we'll impute those
+date[np.isnan(date)] = np.nanmedian(date)
+W = np.stack((np.ones_like(date), date), axis=2)
+fig, axes = plt.subplots(2, 2, figsize=(6, 5), tight_layout=True, sharey="row")
+
+
+# remove quadrats with missing surveys
+is_y_column = mhb_data.columns.str.startswith(f"det{str(year)[2:]}")
+y = mhb_data.loc[:, is_y_column].values
+was_surveyed = ~np.isnan(y).any(axis=1)
+y = y[was_surveyed].astype(int)
+quadrat_count, survey_count = y.shape
+
+# extract our covariates
+elevation = mhb_data["ele"].values[was_surveyed]
+forest_cover = mhb_data["forest"].values[was_surveyed]
+is_date_column = mhb_data.columns.str.startswith(f"date{str(year)[2:]}")
+date = mhb_data.loc[:, is_date_column].values[was_surveyed]
+
+# prepare design matrix for the occurrence model
+elev_scaled = scale(elevation)
+forest_scaled = scale(forest_cover)
+elev_scaled2 = elev_scaled**2
+X = np.column_stack((np.ones_like(elevation), forest_scaled, elev_scaled, elev_scaled2))
+
+# some dates are missing so we'll impute those
+date[np.isnan(date)] = np.nanmedian(date)
+W = np.stack((np.ones_like(date), date), axis=2)
+fig, axes = plt.subplots(2, 2, figsize=(6, 5), tight_layout=True, sharey="row")
 
 ax = axes.flat[0]
 ax.hist(elevation, ec="w", bins=7)
